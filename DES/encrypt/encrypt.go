ddd781ea4ce1ee37f90f0348e29ad0841adbec78
Alexandra <ceobanu_alexandra@yahoo.com>
2024-10-26 13:36:11 +0300

    stergere biti de paritate din key

--- DES/encrypt/encrypt.go
+++ DES/encrypt/encrypt.go
@@ -19,6 +19,28 @@ var Etable = [][]int{
 	{28, 29, 30, 31, 32, 1},
 }
 
+var PC_1 = [][]int{
+	{57, 49, 41, 33, 25, 17, 9},
+	{1, 58, 50, 42, 34, 26, 18},
+	{10, 2, 59, 51, 43, 35, 27},
+	{19, 11, 3, 60, 52, 44, 36},
+	{63, 55, 47, 39, 31, 23, 15},
+	{7, 62, 54, 46, 38, 30, 22},
+	{14, 6, 61, 53, 45, 37, 29},
+	{21, 13, 5, 28, 20, 12, 4},
+}
+
+var PC_2 = [][]int{
+	{14, 17, 11, 24, 1, 5},
+	{3, 28, 15, 6, 21, 10},
+	{23, 19, 12, 4, 26, 8},
+	{16, 7, 27, 20, 13, 2},
+	{41, 52, 31, 37, 47, 55},
+	{30, 40, 51, 45, 33, 48},
+	{44, 49, 39, 56, 34, 53},
+	{46, 42, 50, 36, 29, 32},
+}
+
 func readPlaintext() string {
 	plaintext, error := os.ReadFile("DES/encrypt/plaintext.txt")
 	if error != nil {
@@ -89,6 +111,17 @@ func toBits(arrayOfBytes []byte) string {
 	}
 	return stringOfBits.String()
 }
+
+func deleteBits(bitString string) string {
+
+	var newString strings.Builder
+	for ind := 0; ind < len(bitString); ind = ind + 8 {
+
+		substring := string(bitString[ind : ind+8-1])
+		newString.WriteString(substring)
+	}
+	return newString.String()
+}
 func E(A string) string {
 	var extendedA strings.Builder
 	for i, row := range Etable {
@@ -118,7 +151,14 @@ func encrypt(L0 string, R0 string) {
 
 }
 func main() {
-	// var key = []string{"1", "3", "3", "4", "5", "7", "7", "9", "9", "B", "B", "C", "D", "F", "F", "1"}
+	key := "133457799BBCDFF1"
+	// 00010010011010010101101111001001101101111011011111111000
+	// 00010010011010010101101111001001101101111011011111111000
+	keyArrayOfBytes := toBytes(key)
+	keyStringOfBits := toBits(keyArrayOfBytes)
+	keyStringOfBits = deleteBits(keyStringOfBits)
+	fmt.Println("key bits: ", keyStringOfBits)
+	fmt.Println("-----------------------------------------------")
 	initialP := [][]int{
 		{58, 50, 42, 34, 26, 18, 10, 2},
 		{60, 52, 44, 36, 28, 20, 12, 4},
